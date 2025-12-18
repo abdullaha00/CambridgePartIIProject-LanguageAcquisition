c2063a24c5fa5e319add902b4e02f399fb42e71e
Abdullah Akram <abdullaha3474@gmail.com>
2025-12-18 12:40:25 +0500

    Updated .gitignore and changed parquet structure

--- scripts/Parse_parquet.py
+++ scripts/Parse_parquet.py
@@ -1,28 +1,57 @@
 import pandas as pd
 import os
+from tqdm import tqdm
+
+
+#schema definition
+SCHEMA = {
+    #ids
+    "sentence_id":     "int64", 
+    "user_id":        "string",
+    "tok_id":         "string",
+    
+    # token data
+    "tok":            "string",
+    "pos":            "string", # dropped in minimal
+    "meta":           "string", # dropped in minimal
+    "type":           "string", # dropped in minimal
+    "rt":             "float64", # dropped in minimal
+
+    # user/exercise data
+    "countries":      "string",
+    "client":         "string",
+    "session":        "string",
+    "format":         "string",
+    "days":           "float64",
+    "time":           "float64",
+
+    #label
+    "label":          "Int8",
+
+}
 
 def parse(path: str) -> pd.DataFrame:
-    hasLabel = path.endswith(".train")
+    is_train = path.endswith(".train")
 
     with open(path) as f:
         out = []
-        sentence_idx = -1
+        sentence_idx = 0 if not is_train else -1 #initial prompt missing for dev/test
         for line in f:
             line = line.strip()
             if not line:
                 continue
-            elif line.startswith("# p"): # # prompt:...
+            elif line.startswith("# p"): # line looks like "# prompt:..."
                 sentence_idx += 1
             elif line.startswith("# u"):
                 row = list(map(lambda x: x.split(':'), line.split()))
                 row = [x[1] for x in row[1:]]
-                user, countries, days, client, session, format, time = row
+                user, countries, days, client, session, formt, time = row
 
             else:
                 r=line.split()
                 tokId, tok, pos, meta, typ, rt = r[0:6]
 
-                label = r[-1] if hasLabel else None
+                label = r[-1] if is_train else pd.NA
 
                 #ex = Exercise(tokId, tok, pos, meta, typ, rt, label)
 
@@ -41,7 +70,7 @@ def parse(path: str) -> pd.DataFrame:
                     "days": days,
                     "client": client,
                     "session": session,
-                    "format": format,
+                    "format": formt,
                     "time": time,
                     #Label
                     "label": label
@@ -49,12 +78,58 @@ def parse(path: str) -> pd.DataFrame:
 
         return pd.DataFrame(out)
 
+def parse_key(path:str) -> pd.DataFrame:
+    with open(path) as f:
+        return pd.read_csv(
+            path,
+            sep= " ",
+            names=["tok_id", "label"]
+        )
 
-os.makedirs("parquet", exist_ok=True)
+def merge_with_key(df: pd.DataFrame, df_key: pd.DataFrame) -> pd.DataFrame:
+    
+    df["label"] = df["tok_id"].map(
+        dict(zip(df_key["tok_id"], df_key["label"]))
+    )
 
-for lang in ["en_es", "es_en", "fr_en"]:
-    df = parse(f"data_{lang}/{lang}.slam.20190204.train")
-    #df.to_parquet("parquet/{lang}_original.parquet", index=False)
-    dfM = df[["sentence_id", "user_id", "tok_id", "tok", "countries", "days", "client", "session", "format", "time", "label"]]
-    dfM.to_parquet(f"parquet/{lang}_minimal.parquet", index=False)
+    return df
 
+
+def enforce_schema(df: pd.DataFrame) -> pd.DataFrame:
+    df.replace(to_replace=["null", "", "None"], value=pd.NA, inplace=True)
+
+    for col, dtype in SCHEMA.items():
+        assert col in df.columns
+
+        if dtype in ["string", "boolean"]: #non-numeric
+            df[col] = df[col].astype(dtype)
+        else:
+            df[col] = pd.to_numeric(df[col], errors="coerce").astype(dtype)
+        
+    return df
+
+for lang in tqdm(["en_es", "es_en", "fr_en"], desc="Language"):
+
+    #create folder for parquet, lang combination
+    os.makedirs(f"parquet/{lang}", exist_ok=True)
+
+    for typ in tqdm(["train", "dev", "test"], desc=f"{lang} split", leave=False):
+
+        #------ parse original
+        df = parse(f"data_{lang}/{lang}.slam.20190204.{typ}")
+        
+        # use .key file for dev data
+        if typ == "dev":
+            df_key = parse_key(f"data_{lang}/{lang}.slam.20190204.{typ}.key")
+
+            df = merge_with_key(df, df_key)
+
+        #------ adjust types
+        df = enforce_schema(df)
+
+        #------ 
+        dfM = df.drop(columns=["pos", "type", "meta", "rt"])
+        
+        # save both minimal and original to parquet
+        dfM.to_parquet(f"parquet/{lang}/{typ}/{lang}_{typ}_minimal.parquet", index=False)
+        df.to_parquet(f"parquet/{lang}/{typ}/{lang}_{typ}_original.parquet", index=False)
